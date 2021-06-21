ccfa3bee85a7c7a220f0fae939e3a4f2da5707b3
Pawan <76594354+PkcGhost13@users.noreply.github.com>
2021-06-21 13:47:11 +0530

    2

--- CPPprograms/MatrixMultiplication.cpp
+++ CPPprograms/MatrixMultiplication.cpp
@@ -3,22 +3,34 @@ using namespace std;
 
 int main(void)
 {
-    int r1,r2,c1,c2;
-    cout<<"Enter the dimensions of the 1st matrix :"<<endl;
-    cout<<"Number of rows :  ";
-    cin>>r1;
-    cout<<"\nNumber of columns :  ";
-    cin>>c1;
+    int r1,r2,c1,c2,f=0;
+    cout<<"Note : The no of columns of the 1st matrix should be equal to the number of rows of the second matrix for the matrix multiplication to be valid "<<endl;
+        
+    do
+    {    
+        cout<<"Enter the dimensions of the 1st matrix :"<<endl;
+        cout<<"Number of rows :  ";
+        cin>>r1;
+        cout<<"\nNumber of columns :  ";
+        cin>>c1;
 
-    cout<<"\nEnter The dimensions of the 2nd matrix :"<<endl;
-    cout<<"Number of rows :  ";
-    cin>>r2;
-    cout<<"\nNumber of columns :  ";
-    cin>>c2;
+        cout<<"\nEnter The dimensions of the 2nd matrix :"<<endl;
+        cout<<"Number of rows :  ";
+        cin>>r2;
+        cout<<"\nNumber of columns :  ";
+        cin>>c2;
 
-    cout<<endl;
+        cout<<endl;
+        if(c1!=r2)
+        {
+            cout<<"The matrix multiplication is not posible for the given dimensions. Please try again !!"<<endl;
+        }
+    } 
+    while (c1!=r2);   
 
     int a[r1][c1];
+    int b[r2][c2];
+    int c[r1][c2];
     cout<<"Enter the elements of the first matrix :"<<endl;
     for(int i=0;i<r1;i++)
     {
@@ -28,7 +40,6 @@ int main(void)
         }
     }
 
-    int b[r2][c2];
     cout<<"Enter the elements of the second matrix :"<<endl;
     for(int i=0;i<r2;i++)
     {
@@ -38,6 +49,32 @@ int main(void)
         }
     }
 
-    
+    for(int i=0; i<r1; ++i)
+    {
+         for(int j=0; j<c2; ++j) 
+         {
+            c[i][j] = 0;
+         }
+    }
 
+    for (int i=0;i<r1;i++)
+    {
+        for(int j=0;j<c2;j++)
+        {
+            for(int k=0;k<c1;k++)
+            {
+                c[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+    }
+
+    cout<<"The multiplication of the 2 matrices is:"<<endl;
+    for(int i=0;i<r1;i++)
+    {
+        for(int j=0;j<c2;j++)
+        {
+            cout<<c[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
 }
---
+++ CPPprograms/test1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    show();
+}
+void show()
+{
+    printf("c");
+}
